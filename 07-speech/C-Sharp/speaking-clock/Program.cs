6eab44438c4ad303cf4781769dda62b43aa1bfe0
MohitMehta257 <58682658+MohitMehta257@users.noreply.github.com>
2023-09-05 01:52:56 +0530

    Add files via upload

---
+++ 07-speech/C-Sharp/speaking-clock/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+
+// Import namespaces
+using Microsoft.CognitiveServices.Speech;
+using Microsoft.CognitiveServices.Speech.Audio;
+
+
+namespace speaking_clock
+{
+    class Program
+    {
+        private static SpeechConfig speechConfig;
+        static async Task Main(string[] args)
+        {
+            try
+            {
+                // Get config settings from AppSettings
+                IConfigurationBuilder builder = new ConfigurationBuilder().AddJsonFile("appsettings.json");
+                IConfigurationRoot configuration = builder.Build();
+                string cogSvcKey = configuration["CognitiveServiceKey"];
+                string cogSvcRegion = configuration["CognitiveServiceRegion"];
+
+                // Configure speech service
+
+                speechConfig=SpeechConfig.FromSubscription(cogSvcKey,cogSvcRegion);
+                Console.WriteLine("Ready to use speech Service in "+speechConfig.Region);
+
+                speechConfig.SpeechSynthesisVoiceName="en-US-ArialNeural";
+ 
+                // Get spoken input
+                string command = "";
+                command = await TranscribeCommand();
+                if (command.ToLower()=="what time is it?")
+                {
+                    await TellTime();
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        static async Task<string> TranscribeCommand()
+        {
+            string command = "";
+            
+            // Configure speech recognition
+            using AudioConfig audioConfig=AudioConfig.FromDefaultMicrophoneInput();
+            using SpeechRecognizer speechRecognizer=new SpeechRecognizer(speechConfig,audioConfig);
+            Console.WriteLine("Speak Now...");
+
+            // Process speech input
+
+            SpeechRecognitionResult speech = await speechRecognizer.RecognizeOnceAsync();
+            if(speech.Reason==ResultReason.RecognizedSpeech)
+            {
+                command=speech.Text;
+                Console.WriteLine(command);
+            }
+
+            else
+            {
+                Console.WriteLine(speech.Reason);
+                if(speech.Reason==ResultReason.Canceled)
+                {
+                    var cancellation= CancellationDetails.FromResult(speech);
+                    Console.WriteLine(cancellation.Reason);
+                    Console.WriteLine(cancellation.ErrorDetails);
+                }
+            }
+
+            // Return the command
+            return command;
+        }
+
+        static async Task TellTime()
+        {
+            var now = DateTime.Now;
+            string responseText = "The time is " + now.Hour.ToString() + ":" + now.Minute.ToString("D2");
+                        
+            // Configure speech synthesis
+            speechConfig.SpeechSynthesisVoiceName="en-IN-NeerjaNeural";
+            using SpeechSynthesizer speechSynthesizer=new SpeechSynthesizer(speechConfig);
+
+            // Synthesize spoken output
+            SpeechSynthesisResult speak=await speechSynthesizer.SpeakTextAsync(responseText);
+            if(speak.Reason!=ResultReason.SynthesizingAudioCompleted)
+            {
+                Console.WriteLine(speak.Reason);
+            }
+
+            // Print the response
+            Console.WriteLine(responseText);
+        }
+
+    }
+}
